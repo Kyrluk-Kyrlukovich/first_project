d80494076f580a07d5c8dc011a16e46f7d5c1fe5
Александр <vs.t4werka2004@gmail.com>
2022-11-25 16:07:31 +0500

    search and select

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,122 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>Страница с постами</h1>
+  <div class="app">
+    <my-input 
+      v-model="searchQuery"
+      placeholder="Поиск..."/>
+    <div class="app_btns">
+      <my-button 
+        @click="showDialog">
+        Создать пост
+      </my-button>
+      <my-select 
+        v-model="selectedSort"
+        :options="sortOptions"/>
+    </div>
+    <my-dialog 
+      v-model:show="dialogVisible">
+      <post-form
+        @create="createPost"/>
+    </my-dialog>
+    <post-list 
+    :posts="searchAndSortedPost"
+    @remove="removePost"
+    v-if="!isPostsLoading"/>
+    <div v-else>Идёт загрузка</div>
+  </div>
 </template>
 
+<script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+import axios from "axios";
+import MySelect from './components/UI/MySelect.vue';
+
+export default {
+  components: {
+    PostForm, PostList,
+    MySelect,
+  },
+
+  data() {
+    return {
+      posts: [
+      ],
+
+      dialogVisible: false,
+      isPostsLoading: false,
+      selectedSort: 'standart',
+      searchQuery: '',
+      sortOptions: [
+        {value: 'standart', name: 'Выбери для сортировки', selected: true},
+        {value: 'title', name: 'По названию', selected: false},
+        {value: 'body', name: 'По содежимому', selected: false}
+      ] 
+    }
+  },
+  
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+    },
+
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+
+    showDialog() {
+      this.dialogVisible = true;
+    },
+
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true
+        const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10");
+        this.posts = response.data;
+        console.log(response);
+      } catch (e) {
+        alert("Ошибка")
+      } finally {
+        this.isPostsLoading = false
+      }
+    }
+  },
+
+  mounted() {
+    this.fetchPosts();
+  },
+
+  computed: {
+    sortedPost() {
+      if(this.selectedSort === 'standart') {
+        return [...this.posts]
+      } else {
+        return [...this.posts].sort( (post1, post2) => post1[this.selectedSort].localeCompare(post2[this.selectedSort]))
+      }      
+    },
+
+    searchAndSortedPost() {
+      return this.sortedPost.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  box-sizing: border-box;
+  padding: 0;
 }
 
-nav {
-  padding: 30px;
+.app {
+  padding: 20px
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app_btns {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
 }
 </style>
---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,45 @@
+<template>
+  <select 
+    class="select"
+    :value="modelValue" 
+    @change="changeOptions">
+    <option 
+      v-for="option in options" 
+      :key="option.value" 
+      :value="option.value"
+      :selected="option.selected">
+    {{option.name}}
+  </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'my-select',
+
+  props: {
+    modelValue: {
+      type: String
+    },
+    options: {
+      type: Array,
+      default:() => []
+    }
+  },
+
+  methods: {
+    changeOptions(event) {
+      this.$emit('update:modelValue', event.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+.select {
+  background: none;
+  border: 1px solid teal;
+  color: teal;
+  padding: 5px;
+}
+</style>
